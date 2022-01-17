4369c24613ab3f6d50a54264389357677ee0736e
yomioakande <akandeabayomi60@gmail.com>
2022-01-17 17:41:00 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+// Sticky Nav
+$(function () {
+  function sticky() {
+    if ($(window).scrollTop() >= 30) {
+      $(".nav-header").addClass("sticky");
+    } else if ($(window).scrollTop() < 30) {
+      $(".nav-header").removeClass("sticky");
+    }
+  }
+  $(window).on("scroll", function () {
+    sticky();
+  });
+  $(window).on("load", sticky());
+});
+
+// Navigation Button
+$(document).mouseup(function (e) {
+  var fnav = $(".navigation");
+  if (!fnav.is(e.target)) {
+    $(".navigation").removeClass("show");
+    $(".menu-icon").removeClass("open");
+    $(".nav-header").removeClass("shadow");
+  }
+  $(".menu-icon").on("click", function () {
+    $(this).toggleClass("open");
+    $(".navigation").toggleClass("show");
+    $(".nav-header").toggleClass("shadow");
+  });
+});
